0b5e25ca828011f35ca20dc732df171544f5cde9
Ikwillyou <42628615+Ikwillyou@users.noreply.github.com>
2021-06-10 13:42:44 -0400

    First dataset added
    mental-health-in-tech-2016.csv
    
    also added numpy at the top as np

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+import numpy as np
+
 print("HELLO WORLD")
 print("NATHANIEL")
 
