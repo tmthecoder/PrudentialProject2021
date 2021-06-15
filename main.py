a2ac19a7829dcc4394c16a34dd3dbeef3dc3221b
Ikwillyou <42628615+Ikwillyou@users.noreply.github.com>
2021-06-15 13:33:52 -0400

    file write to txt

--- main.py
+++ main.py
@@ -17,3 +17,9 @@ for x in mentalhealthtech1["Do you currently have a mental health disorder?"]:
         responseMappings[x] = 0
 print("Response Counts: %s" % responseMappings)
 print("Total Responses: %s" % mentalhealthtech1["Do you currently have a mental health disorder?"].count())
+
+f = open("datasets/Do_you_currently_have_a_mental_health_disorder.txt", "w")
+f.write("Response Counts: %s" % responseMappings)
+f.write("Total Responses: %s" % mentalhealthtech1["Do you currently have a mental health disorder?"].count())
+
+
